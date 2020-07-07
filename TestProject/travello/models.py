9ad27a74c04322041fc7548466f0b96feaf610ac
Alimov-8 <Alimov_Abdulloh2001@mail.ru>
2020-07-07 14:38:58 +0500

    Sixth Commit DjAdmin

--- TestProject/travello/models.py
+++ TestProject/travello/models.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
     name =  models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics') 
     desc = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
