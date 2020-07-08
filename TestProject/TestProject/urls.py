0584a6d1fcc3f742fa5debf6ecb2163f3e3821d9
Alimov-8 <Alimov_Abdulloh2001@mail.ru>
2020-07-08 15:30:57 +0500

    9th Commit, User Register

--- TestProject/TestProject/urls.py
+++ TestProject/TestProject/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('travello.urls')), 
+    path('accounts/', include('accounts.urls')), 
     path('admin/', admin.site.urls),
 ]
 
