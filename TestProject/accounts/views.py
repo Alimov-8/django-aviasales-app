b5f24c4c05586f08422e71636e6d4b5a1b26cf76
Alimov-8 <Alimov_Abdulloh2001@mail.ru>
2020-07-10 11:10:28 +0500

    10th Commit, Project finished, bugs+

--- TestProject/accounts/views.py
+++ TestProject/accounts/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
+def login(request):
+    if request.method == 'Post':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -11,9 +26,11 @@ def register(request):
         password2 = request.POST['password2']
         if password1 == password2:
             if User.objects.filter(username=username).exits():
-                print('Username Taken')
+                messages.info(request, ' Username Taken ')
+                return redirect('register')
             elif User.objects.filter(email=email).exits():
-                print('Email Taken')    
+                messages.info(request, ' Email Taken ')
+                return redirect('register')    
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name,last_name=last_name)
                 user.save()
